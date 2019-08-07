fe8db94432d1fbac7da6fc23976371f1273e9e5d
Fabian Andiel <fabian.andiel@yahoo.com>
2019-08-07 09:18:36 +0200

    Adds landing page, database with all tables without test data, trainer login draft

---
+++ landing.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.7/css/mdb.min.css" rel="stylesheet">
+
+
+
+	<style type="text/css">
+
+
+		body {
+		  background-image: url('images/landing_img.jpg');
+		  background-repeat: no-repeat;
+		  background-size:cover;
+		  background-position:center center;  
+		  background-attachment: fixed;	
+
+		}
+
+		#landing_row_hor {
+			display:flex;
+			justify-content: center;
+		}
+
+		#landing_row_ver {
+			width:400px;
+			height:100px;
+		/*	border:2px solid red;*/
+			height:745px;
+
+			display: flex;
+			align-items: center;
+
+			
+			
+		}
+
+		#test {
+			width:400px;
+			height:200px;
+			/*border:2px solid green;*/
+
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+		}
+
+		.landing_link_container {
+			text-align: center;
+		}
+		
+		a {
+			color:white;
+			font-weight: bold;
+			font-size: 40px;
+		}
+
+	</style>
+</head>
+<body>
+
+	<div class="container">
+
+	<!-- <a href=""> Enter as a customer </a>
+
+	<a href=""> Enter as a trainer </a> -->
+
+		<div id="landing_row_hor">
+			<div id="landing_row_ver" class="">
+				<div id="test" class="mt-5">
+				
+					<div class="landing_link_container"> 
+						<a href=""> Enter as a student</a>
+					</div>
+
+					<div class="landing_link_container"> 
+						<a href=""> Enter as a coach</a>
+					</div>
+				</div>	
+			</div>
+		</div>
+
+
+
+
+
+	</div>
+
+
+
+
+	
+
+	
+
+
+
+
+
+
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.7/js/mdb.min.js"></script>
+	
+
+</body>
+</html>
